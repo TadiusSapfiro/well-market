078f111c8cfb5a1a211eadfa64d0200a4c232244
Владимир <v.zelentinov@gmail.com>
2019-04-05 14:27:59 +0300

    заончил верстку, тлаживаю

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-});
+// id="place1"
+
+// id="distanse1"
+
+// id="soil"
+
+// id="rings"
+
+// id="filter"
+
+// id="hоuse"
+
+// id="ch-1"
+
+// id="ch-2"
+
+// id="ch-3"
+
+// id="ch-4"
+
+var distanse =  document.getElementById('distanse1').value;
+
+var distansePrice = 0;
+
+if (distanse >= 2000) {
+	distansePrice = 500
+}
+
+console.log(distansePrice);
